615d00661e18cf5ccce8bd1b23a6e0a0ca675500
wangkc <wkclz@qq.com>
2025-12-13 11:59:30 +0800

    优化配置

--- sh-mybatis/src/main/java/com/wkclz/mybatis/ShMyBatisAutoConfig.java
+++ sh-mybatis/src/main/java/com/wkclz/mybatis/ShMyBatisAutoConfig.java
@@ -1,6 +1,5 @@
 package com.wkclz.mybatis;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
