7dc332d8d5780a178f0304f12cbcfcfa939b0c0f
wangkc <wkclz@qq.com>
2025-12-13 17:36:35 +0800

    增加 redis 模块

---
+++ sh-redis/src/main/java/com/wkclz/redis/ShRedisAutoConfig.java
@@ -0,0 +1,11 @@
+package com.wkclz.redis;
+
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+@AutoConfiguration
+@ComponentScan(basePackages = {"com.wkclz.redis"})
+public class ShRedisAutoConfig {
+}
+
+
