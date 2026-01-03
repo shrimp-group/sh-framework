b5746424003322f1341f42ebe3e22daa1309ae86
wangkc <wkclz@qq.com>
2026-01-04 01:06:31 +0800

    动态数据源模块

---
+++ sh-dynamicdb/src/main/java/com/wkclz/dynamicdb/ShDynamicdbAutoConfig.java
@@ -0,0 +1,11 @@
+package com.wkclz.dynamicdb;
+
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+@AutoConfiguration
+@ComponentScan(basePackages = {"com.wkclz.dynamicdb"})
+public class ShDynamicdbAutoConfig {
+}
+
+
