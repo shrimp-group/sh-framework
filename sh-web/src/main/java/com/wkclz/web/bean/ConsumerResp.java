4b00a73d5992a1111ba57f78f6ade82298a441e9
shrimp <shrimp@wkclz.com>
2026-08-16 16:21:27 +0800

    C端返回父类

---
+++ sh-web/src/main/java/com/wkclz/web/bean/ConsumerResp.java
@@ -0,0 +1,18 @@
+package com.wkclz.web.bean;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * C 端响应基类（面向小程序/H5/App 等消费者端，仅含主键，避免带出审计字段）
+ */
+@Data
+@Schema(description = "C 端响应基类")
+public class ConsumerResp implements Serializable {
+
+    @Schema(description = "主键ID")
+    private Long id;
+
+}
--- sh-web/src/main/java/com/wkclz/web/bean/RemoveReq.java
+++ sh-web/src/main/java/com/wkclz/web/bean/RemoveReq.java
@@ -7,7 +7,6 @@ import lombok.Data;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 @Data
 @Schema(description = "删除请求")
